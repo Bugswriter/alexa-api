513937248e71ec00de0853b48268c01d0af6cb58
bugswriter <surajkushwah6969@gmail.com>
2020-07-27 13:09:55 +0530

    database now in cloud elephantsql added

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./site.db'
-engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False} )
+SQLALCHEMY_DATABASE_URL = 'postgres://xtezipxz:3twTyHrhvQ-nXKoWeLuilGej3AaNjsCE@satao.db.elephantsql.com:5432/xtezipxz'
+engine = create_engine( SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
