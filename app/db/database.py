656fc6fb1b16f5685be96eedd27483e45c1afdfd
bugswriter <surajkushwah6969@gmail.com>
2020-07-28 13:07:42 +0530

    wapas sqlite

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = 'postgres://xtezipxz:3twTyHrhvQ-nXKoWeLuilGej3AaNjsCE@satao.db.elephantsql.com:5432/xtezipxz'
-engine = create_engine( SQLALCHEMY_DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./site.db"
+#SQLALCHEMY_DATABASE_URL = 'postgres://xtezipxz:3twTyHrhvQ-nXKoWeLuilGej3AaNjsCE@satao.db.elephantsql.com:5432/xtezipxz'
+engine = create_engine( SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
